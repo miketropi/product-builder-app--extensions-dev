40b6606678d552c92b7ee449558ae490c2143e89
quocvietht03 <quocvietht03@gmail.com>
2024-05-21 15:27:33 +0700

    update layout menu builder

--- theme-extension/assets/product-builder.bundle.js
+++ theme-extension/assets/product-builder.bundle.js
@@ -789,7 +789,7 @@ function MenuBuilderApp() {
     version = _useMenuBuilderContex.version,
     menuData = _useMenuBuilderContex.menuData;
   var renderMenu = function renderMenu(menu) {
-    var _parent_item$config, _parent_item$config3, _parent_item$config4, _parent_item$config5;
+    var _parent_item$config, _parent_item$config3, _parent_item$config4, _parent_item$config5, _window;
     var lv = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
     var __parent_item = arguments.length > 2 ? arguments[2] : undefined;
     lv = lv === null ? 0 : lv += 1;
@@ -807,9 +807,11 @@ function MenuBuilderApp() {
             children: __parent_item.name
           }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("div", {
             className: "__custom-link",
-            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("a", {
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("a", {
               href: __parent_item.config.custom_url,
-              children: __parent_item.config.custom_text
+              children: [__parent_item.config.custom_text, /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_MenuIcon__WEBPACK_IMPORTED_MODULE_2__["default"], {
+                source: 'arrow_next'
+              })]
             })
           })]
         })
@@ -857,9 +859,9 @@ function MenuBuilderApp() {
       var _parent_item$config2;
       containerStyle.padding = __parent_item === null || __parent_item === void 0 || (_parent_item$config2 = __parent_item.config) === null || _parent_item$config2 === void 0 ? void 0 : _parent_item$config2.container_padding;
     }
-    var __ul = /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("ul", {
+    var __ul = /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("ul", {
       className: classesUl.join(' '),
-      children: (__parent_item === null || __parent_item === void 0 || (_parent_item$config3 = __parent_item.config) === null || _parent_item$config3 === void 0 ? void 0 : _parent_item$config3.container) == true ? /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
+      children: [(__parent_item === null || __parent_item === void 0 || (_parent_item$config3 = __parent_item.config) === null || _parent_item$config3 === void 0 ? void 0 : _parent_item$config3.container) == true ? /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
         className: "__container-item",
         children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("div", {
           className: "__container-item-inner",
@@ -874,7 +876,11 @@ function MenuBuilderApp() {
             })]
           })
         })]
-      }) : __li
+      }) : __li, (__parent_item === null || __parent_item === void 0 ? void 0 : __parent_item.type) && __parent_item.type == '__MEGASHOP__' && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("li", {
+        dangerouslySetInnerHTML: {
+          __html: (_window = window) === null || _window === void 0 ? void 0 : _window.__custom_html
+        }
+      })]
     });
     return __ul;
   };
@@ -883,15 +889,8 @@ function MenuBuilderApp() {
     children: [menuData && menuData.length > 0 && renderMenu(menuData), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("a", {
       href: "#",
       "class": "menu-builder-mobi__open",
-      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("svg", {
-        width: "12",
-        height: "12",
-        viewBox: "0 0 12 12",
-        fill: "none",
-        xmlns: "http://www.w3.org/2000/svg",
-        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("path", {
-          d: "M11.88 0H0.12C0.054 0 0 0.0613636 0 0.136364V1.22727C0 1.30227 0.054 1.36364 0.12 1.36364H11.88C11.946 1.36364 12 1.30227 12 1.22727V0.136364C12 0.0613636 11.946 0 11.88 0ZM11.88 10.6364H0.12C0.054 10.6364 0 10.6977 0 10.7727V11.8636C0 11.9386 0.054 12 0.12 12H11.88C11.946 12 12 11.9386 12 11.8636V10.7727C12 10.6977 11.946 10.6364 11.88 10.6364ZM11.88 5.31818H0.12C0.054 5.31818 0 5.37955 0 5.45455V6.54545C0 6.62045 0.054 6.68182 0.12 6.68182H11.88C11.946 6.68182 12 6.62045 12 6.54545V5.45455C12 5.37955 11.946 5.31818 11.88 5.31818Z"
-        })
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_MenuIcon__WEBPACK_IMPORTED_MODULE_2__["default"], {
+        source: 'menu_toggle'
       })
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_MenuMobi__WEBPACK_IMPORTED_MODULE_3__["default"], {})]
   });
@@ -913,10 +912,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 
 var __ICONS = {
-  'HB': "<svg width=\"12\" height=\"12\" viewBox=\"0 0 12 12\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"> <path d=\"M11.88 0H0.12C0.054 0 0 0.0613636 0 0.136364V1.22727C0 1.30227 0.054 1.36364 0.12 1.36364H11.88C11.946 1.36364 12 1.30227 12 1.22727V0.136364C12 0.0613636 11.946 0 11.88 0ZM11.88 10.6364H0.12C0.054 10.6364 0 10.6977 0 10.7727V11.8636C0 11.9386 0.054 12 0.12 12H11.88C11.946 12 12 11.9386 12 11.8636V10.7727C12 10.6977 11.946 10.6364 11.88 10.6364ZM11.88 5.31818H0.12C0.054 5.31818 0 5.37955 0 5.45455V6.54545C0 6.62045 0.054 6.68182 0.12 6.68182H11.88C11.946 6.68182 12 6.62045 12 6.54545V5.45455C12 5.37955 11.946 5.31818 11.88 5.31818Z\"/> </svg>",
-  'arrow_down': "<svg viewBox=\"0 0 1024 1024\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M759.2 419.8L697.4 358 512 543.4 326.6 358l-61.8 61.8L512 667z\"/></svg>",
-  'arrow_next': "<svg viewBox=\"0 0 1024 1024\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M419.3 264.8l-61.8 61.8L542.9 512 357.5 697.4l61.8 61.8L666.5 512z\"/></svg>",
-  'arrow_back': "<svg viewBox=\"0 0 1024 1024\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M604.7 759.2l61.8-61.8L481.1 512l185.4-185.4-61.8-61.8L357.5 512z\"/></svg>"
+  'HB': "<svg width=\"12\" height=\"12\" viewBox=\"0 0 12 12\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M11.88 0H0.12C0.054 0 0 0.0613636 0 0.136364V1.22727C0 1.30227 0.054 1.36364 0.12 1.36364H11.88C11.946 1.36364 12 1.30227 12 1.22727V0.136364C12 0.0613636 11.946 0 11.88 0ZM11.88 10.6364H0.12C0.054 10.6364 0 10.6977 0 10.7727V11.8636C0 11.9386 0.054 12 0.12 12H11.88C11.946 12 12 11.9386 12 11.8636V10.7727C12 10.6977 11.946 10.6364 11.88 10.6364ZM11.88 5.31818H0.12C0.054 5.31818 0 5.37955 0 5.45455V6.54545C0 6.62045 0.054 6.68182 0.12 6.68182H11.88C11.946 6.68182 12 6.62045 12 6.54545V5.45455C12 5.37955 11.946 5.31818 11.88 5.31818Z\"/> </svg>",
+  'arrow_down': "<svg width=\"12\" height=\"8\" viewBox=\"0 0 12 8\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1 2.5L6 7.5L11 2.5\" stroke=\"currentColor\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>",
+  'arrow_next': "<svg width=\"20\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M7 16L13 10L7 4\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>",
+  'arrow_back': "<svg width=\"8\" height=\"14\" viewBox=\"0 0 8 14\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M7 1L1 7L7 13\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>",
+  'menu_toggle': "<svg xmlns=\"http://www.w3.org/2000/svg\" aria-hidden=\"true\" focusable=\"false\" class=\"icon icon-hamburger\" fill=\"none\" viewBox=\"0 0 18 16\"><path d=\"M1 .5a.5.5 0 100 1h15.71a.5.5 0 000-1H1zM.5 8a.5.5 0 01.5-.5h15.71a.5.5 0 010 1H1A.5.5 0 01.5 8zm0 7a.5.5 0 01.5-.5h15.71a.5.5 0 010 1H1a.5.5 0 01-.5-.5z\" fill=\"currentColor\"></path></svg>",
+  'menu_close': "<svg width=\"20\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M0.833496 0.833008L19.1666 19.1662\" stroke=\"#19242D\" stroke-width=\"1.5\"/><path d=\"M19.1665 0.833008L0.833434 19.1662\" stroke=\"#19242D\" stroke-width=\"1.5\"/></svg>"
 };
 function MenuIcon(_ref) {
   var source = _ref.source,
@@ -970,7 +971,7 @@ function MenuMobi() {
     document.body.appendChild(wrapEl.current);
   }, []);
   var renderMenuMobi = function renderMenuMobi(menu) {
-    var _parentItem$config, _parentItem$config2, _parentItem$config3, _parentItem$config4, _parentItem$config5, _parentItem$config6;
+    var _parentItem$config, _parentItem$config2, _parentItem$config3, _parentItem$config4, _parentItem$config5, _parentItem$config6, _window;
     var lv = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
     var __parent_item = arguments.length > 2 ? arguments[2] : undefined;
     lv = lv === null ? 0 : lv += 1;
@@ -997,9 +998,11 @@ function MenuMobi() {
           },
           children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("h4", {
             children: parentItem === null || parentItem === void 0 ? void 0 : parentItem.name
-          }), (parentItem === null || parentItem === void 0 || (_parentItem$config2 = parentItem.config) === null || _parentItem$config2 === void 0 ? void 0 : _parentItem$config2.custom_text) && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("a", {
+          }), (parentItem === null || parentItem === void 0 || (_parentItem$config2 = parentItem.config) === null || _parentItem$config2 === void 0 ? void 0 : _parentItem$config2.custom_text) && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("a", {
             href: parentItem === null || parentItem === void 0 || (_parentItem$config3 = parentItem.config) === null || _parentItem$config3 === void 0 ? void 0 : _parentItem$config3.custom_url,
-            children: parentItem === null || parentItem === void 0 || (_parentItem$config4 = parentItem.config) === null || _parentItem$config4 === void 0 ? void 0 : _parentItem$config4.custom_text
+            children: [parentItem === null || parentItem === void 0 || (_parentItem$config4 = parentItem.config) === null || _parentItem$config4 === void 0 ? void 0 : _parentItem$config4.custom_text, /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_MenuIcon__WEBPACK_IMPORTED_MODULE_3__["default"], {
+              source: 'arrow_next'
+            })]
           })]
         })
       }), menu.map(function (item, __i_index) {
@@ -1102,16 +1105,16 @@ function MenuMobi() {
             href: parentItem === null || parentItem === void 0 || (_parentItem$config5 = parentItem.config) === null || _parentItem$config5 === void 0 ? void 0 : _parentItem$config5.container_bottom_custom_url,
             children: [parentItem === null || parentItem === void 0 || (_parentItem$config6 = parentItem.config) === null || _parentItem$config6 === void 0 ? void 0 : _parentItem$config6.container_bottom_custom_text, /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("span", {
               "class": "__menu-icon ",
-              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("svg", {
-                viewBox: "0 0 1024 1024",
-                xmlns: "http://www.w3.org/2000/svg",
-                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("path", {
-                  d: "M419.3 264.8l-61.8 61.8L542.9 512 357.5 697.4l61.8 61.8L666.5 512z"
-                })
+              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_MenuIcon__WEBPACK_IMPORTED_MODULE_3__["default"], {
+                source: 'arrow_next'
               })
             })]
           })
         })
+      }), jumpDeep.length == 0 && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("li", {
+        dangerouslySetInnerHTML: {
+          __html: (_window = window) === null || _window === void 0 ? void 0 : _window.__custom_html
+        }
       })]
     });
     var __ul = /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("ul", {
@@ -1176,21 +1179,8 @@ function MenuMobi() {
             }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("a", {
               href: "#",
               "class": "menu-builder-mobi__close",
-              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("svg", {
-                width: "20",
-                height: "20",
-                viewBox: "0 0 20 20",
-                fill: "none",
-                xmlns: "http://www.w3.org/2000/svg",
-                children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("path", {
-                  d: "M0.833496 0.833008L19.1666 19.1662",
-                  stroke: "#19242D",
-                  "stroke-width": "1.5"
-                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("path", {
-                  d: "M19.1665 0.833008L0.833434 19.1662",
-                  stroke: "#19242D",
-                  "stroke-width": "1.5"
-                })]
+              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_MenuIcon__WEBPACK_IMPORTED_MODULE_3__["default"], {
+                source: 'menu_close'
               })
             })]
           }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("div", {
